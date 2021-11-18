3c54020d6de53998e205fdec50a51a8fbcb218e6
andrewcse <andrewdc@uw.edu>
2021-11-18 06:39:28 -0800

    all charts completed

--- chart2.R
+++ chart2.R
@@ -4,7 +4,8 @@ library(gridExtra)
 
 states_all <- read.csv("https://raw.githubusercontent.com/andrewdcse/Exploratory-Analysis-andrewdcse/main/states_all.csv")
 
-states_tested <- filter(na.omit(states_all), YEAR == 2003 || YEAR == 2005 || YEAR == 2007 || YEAR == 2009 || YEAR == 2011 || YEAR == 2013 || YEAR == 2015)
+states_tested <- filter(na.omit(states_all), 
+                        YEAR == 2003 || YEAR == 2005 || YEAR == 2007 || YEAR == 2009 || YEAR == 2011 || YEAR == 2013 || YEAR == 2015)
 
 states_tested <- mutate(states_tested, expenditure_per_student = TOTAL_EXPENDITURE / ENROLL, 
                         avg_score = (AVG_MATH_4_SCORE + AVG_MATH_8_SCORE + AVG_READING_4_SCORE + AVG_READING_8_SCORE)/4)
@@ -13,16 +14,13 @@ states_tested_years <- group_by(states_tested, YEAR)
 
 states_tested_years_avg <- summarize(states_tested_years, avg_expenditure = mean(expenditure_per_student), avg_score = mean(avg_score))
 
-exp_color <-"#69b3a2"
-score_color <- "#042069"
-
 expenditure_time <- ggplot(data=states_tested_years_avg) + geom_line(aes(x=YEAR, y=avg_expenditure)) + 
   labs(title="Average State Per-Student Expenditure 2003-2015", x="Year", y="Average Expenditure Per Student") +
   scale_x_continuous(breaks=c(2003,2005,2007,2009,2011,2013,2015)) +
   theme(title=element_text(size=8), axis.title = element_text(size = 8))
 
 scores_time <- ggplot(data=states_tested_years_avg) + geom_line(aes(x=YEAR, y=avg_score)) + 
-  labs(title="Average National NAEM Scores 2003-2015", x="Year", y="National Average NAEM Score") +
+  labs(title="Average National NAEP Scores 2003-2015", x="Year", y="National Average NAEP Score") +
   scale_x_continuous(breaks=c(2003,2005,2007,2009,2011,2013,2015)) +
   theme(title=element_text(size=8), axis.title = element_text(size = 8))
 
