ccfd8c2b3d209cc33b28f5e6524a469d42eb4547
Jbhamipur <jsnanak@uw.edu>
2021-11-19 13:29:42 -0800

    organized and made changes

--- Aggregate_Table.R
+++ Aggregate_Table.R
@@ -8,7 +8,7 @@ states_all <- read.csv("https://raw.githubusercontent.com/andrewdcse/Exploratory
 
 states_table <- states_all %>%
   select (STATE, YEAR, TOTAL_REVENUE, TOTAL_EXPENDITURE, GRADES_ALL_G,
-          AVG_READING_4_SCORE, AVG_READING_8_SCORE, AVG_MATH_4_SCORE, AVG_MATH_8_SCORE)
-rename(states_table, "ALL_STUDENTS" = "GRADES_ALL_G")
+          AVG_READING_4_SCORE, AVG_READING_8_SCORE, AVG_MATH_4_SCORE, AVG_MATH_8_SCORE) 
+states_table <- rename(states_table, "ALL_STUDENTS" = "GRADES_ALL_G")
   
 
