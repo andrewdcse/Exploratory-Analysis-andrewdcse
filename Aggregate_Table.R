55d6bbf77e49294bbe3441d726bf21e5965c1960
Jbhamipur <jsnanak@uw.edu>
2021-11-19 14:31:57 -0800

    added comments and made changes

--- Aggregate_Table.R
+++ Aggregate_Table.R
@@ -8,7 +8,21 @@ states_all <- read.csv("https://raw.githubusercontent.com/andrewdcse/Exploratory
 
 states_table <- states_all %>%
   select (STATE, YEAR, TOTAL_REVENUE, TOTAL_EXPENDITURE, GRADES_ALL_G,
-          AVG_READING_4_SCORE, AVG_READING_8_SCORE, AVG_MATH_4_SCORE, AVG_MATH_8_SCORE) 
+          AVG_READING_4_SCORE, AVG_READING_8_SCORE, AVG_MATH_4_SCORE, AVG_MATH_8_SCORE)
+# renaming a column to clarify the column
 states_table <- rename(states_table, "ALL_STUDENTS" = "GRADES_ALL_G")
   
+
+
+
+
+##Why did you include the table? 
+## The table was included to represent the data in a more organized and condensed manner, 
+## only including the columns that are needed or need to be specified.
+##
+##What information does it reveal?
+## The information that the table reveals includes records of the columns. 
+## The columns include state, year, total revenue, total expenditure, 
+## all students in the state, average reading 4 score, average reading 8 score, 
+## average math 4 score, average math 8 score
 
